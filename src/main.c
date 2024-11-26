463afb5e5b7b2852d3cdeb1ab9de2970e9fb8b1b
zedzee <carsonuderitz@gmail.com>
2024-11-26 15:30:22 -0500

    fix main

--- src/main.c
+++ src/main.c
@@ -1,10 +1,5 @@
-#include "board.h"
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdio.h>
 
 int main() {
-	Board board = board_init();
-	uint64_t pos = 1ULL << 42;
-	uint64_t piece = board_get_moves(board, pos, W_KNIGHT_IDX);
 }
